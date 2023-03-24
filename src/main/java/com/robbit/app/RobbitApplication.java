774306dc11a06efc7957d4a1d76885194bcc11d5
Happychoco5 <theendereye@yahoo.com>
2023-03-23 22:56:12 -0700

    Initial commit

---
+++ src/main/java/com/robbit/app/RobbitApplication.java
@@ -0,0 +1,13 @@
+package com.robbit.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RobbitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RobbitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/robbit/app/RobbitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.robbit.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RobbitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
